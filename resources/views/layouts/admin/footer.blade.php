b9c13bd2ee5d26c663b23ca06d0736e0864a2a08
ayusara <ayu24si@mahasiswa.pcr.ac.id>
2025-12-14 14:07:59 +0700

    penambahan footer

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -1,3 +1,12 @@
+<footer class="text-center py-3 mt-4">
+                <small class="text-muted">
+                    © {{ date('Y') }} Sistem Pembangunan dan Monitoring Proyek · All Rights Reserved
+                </small>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <!-- Script -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
